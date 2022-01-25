d3669143dd677fe43cfd42e1a042a1f5fa07efb4
Alana Lipkin <alana.lipkin@gmail.com>
2022-01-25 12:32:19 -0500

    added components, contexts, pages files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useContext } from 'react'
 //components
 import Nav from './components/Nav'
+//contexts
+import UserContext from './contexts/UserContex';
+//css
 import './App.css';
 
 function App() {
+  // in able for us to use our context, we import first, then we can use the useContext hook to access our context
+  //const user = useContext(UserContext)
+  //console.log(user)
   return (
     <div className="App">
+   
+{/* All context comes with the Provider Component. this allows us to use this as a wrapper and share the information to all of it's children. We need the value prop inside our provider.*/}
+  <UserContext.Provider value={'Teo'}>
       <Nav />
+  </UserContext.Provider>
+  
+
+
     </div>
   );
 }
---
+++ src/contexts/UserContex.js
@@ -0,0 +1,8 @@
+//import createContext
+//import React, { createContext } from 'react'
+import { createContext } from 'react'
+//Simplest way to create a context
+//Standard practice: name context the same as your file
+const UserContext = createContext(null)
+
+export default UserContext
