976e0d94eb889678f31aab79128450c18ecfe3c6
Alana Lipkin <alana.lipkin@gmail.com>
2022-01-25 15:42:44 -0500

    updated user context inside login component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useState, useContext } from 'react'
 import { Routes, Route } from 'react-router-dom'
 //components
 import Nav from './components/Nav'
@@ -13,18 +13,20 @@ function App() {
   // in able for us to use our context, we import first, then we can use the useContext hook to access our context
   //const user = useContext(UserContext)
   //console.log(user)
+  const [user, setUser] = useState('')
+
   return (
     <div className="App">
    
 {/* All context comes with the Provider Component. this allows us to use this as a wrapper and share the information to all of it's children. We need the value prop inside our provider.*/}
-  <UserContext.Provider value={'Christina'}>
+  <UserContext.Provider value={ user }>
       <Nav />
 
       {/*  We need to wrap all of our routes inside react reouter Routes component */}
 
-      
+
       <Routes>
-        <Route path='login' element={<Login />} />
+        <Route path='login' element={<Login setUser={setUser} />} />
       </Routes>
 
 
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,25 +1,65 @@
-import React from 'react';
-import './styles.css'
 
-const Index = () => {
-    return (
-        <form className='mx-auto border p-2 m-2' id='login-form'>
-  <div className="mb-3">
-    <label htmlFor="exampleInputuser1" className="form-label">username address</label>
-    <input type="text" className="form-control" id="exampleInputuser1" aria-describedby="userHelp" />
-    <div id="userHelp" className="form-text">We will lie to you.</div>
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" />
-  </div>
-  <div className="mb-3 form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
-    );
-}
+import { useState} from "react";
+import "./styles.css";
 
-export default Index;
+const Login = ({ setUser }) => {
+  // Once the user clicks submit, we will mimic log in and conditionally render our nav bar.
+
+  const [username, setUsername] = useState('')
+
+  const handleChange = e => {
+      setUsername(e.target.value)
+  }
+
+  const handleSubmit = e => {
+      e.preventDefault()
+      setUser(username)
+  }
+
+//   console.log('props', props.setUser())
+  return (
+    <form className="mx-auto border p-2 m-2" id="login-form" onSubmit= {handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="exampleInputuser1" className="form-label">
+          Username
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="exampleInputuser1"
+          aria-describedby="userHelp"
+          value={username}
+          onChange={handleChange}
+        />
+        <div id="userHelp" className="form-text">
+          We will lie to you.
+        </div>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+      <div className="mb-3 form-check">
+        <input
+          type="checkbox"
+          className="form-check-input"
+          id="exampleCheck1"
+        />
+        <label className="form-check-label" htmlFor="exampleCheck1">
+          Check me out
+        </label>
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Login;
